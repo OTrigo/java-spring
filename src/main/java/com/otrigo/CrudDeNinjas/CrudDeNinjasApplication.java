bc1ca1520f4e83f39213c07fbd8bebefc966bb26
Jorge <jorgelucastcoronado@gmail.com>
2025-03-27 18:30:12 -0300

    feat: initializing the project

---
+++ src/main/java/com/otrigo/CrudDeNinjas/CrudDeNinjasApplication.java
@@ -0,0 +1,13 @@
+package com.otrigo.CrudDeNinjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudDeNinjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDeNinjasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/otrigo/CrudDeNinjas/CrudDeNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otrigo.CrudDeNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDeNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
