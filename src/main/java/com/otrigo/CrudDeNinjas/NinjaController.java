dc4b7f2d2d54841a8c812bc207084ee615cef3f2
Jorge <jorgelucastcoronado@gmail.com>
2025-03-28 20:23:36 -0300

    first entity with jpa

--- src/main/java/com/otrigo/CrudDeNinjas/NinjaController.java
+++ src/main/java/com/otrigo/CrudDeNinjas/NinjaController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class Controller {
+public class NinjaController {
     @GetMapping("/rotaget")
      public String welcome(){
         return "Essa é a minha primeira mensagem";
